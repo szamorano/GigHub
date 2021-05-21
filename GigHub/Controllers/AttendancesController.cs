51d27bf9e87a607703c6c6e417d6722ea08a3f8f
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-05-20 23:59:27 -0400

    Building the API

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        public IHttpActionResult Attend(int gigId)
+        {
+            var attendance = new Atendance
+        }
+    }
+}
