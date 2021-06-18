e18d0bbbdd434b2935ec686791704888a7aab82f
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-06-17 21:35:36 -0400

    Build the cancel API

---
+++ GigHub/Controllers/Api/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private ApplicationDbContext _db;
+
+        public GigsController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _db.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            gig.IsCanceled = true;
+            _db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
