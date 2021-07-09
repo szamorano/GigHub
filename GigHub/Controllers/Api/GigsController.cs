4cbe104c18f464e5590fd9c4732ded61238850a9
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-07-08 21:09:14 -0400

    Create and store notifications for canceled gigs

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -26,27 +26,27 @@ namespace GigHub.Controllers.Api
 
             gig.IsCanceled = true;
 
-            //var notification = new Notification
-            //{
-            //    DateTime = DateTime.Now,
-            //    Gig = gig,
-            //    Type = NotificationType.GigCanceled
-            //};
+            var notification = new Notification
+            {
+                DateTime = DateTime.Now,
+                Gig = gig,
+                Type = NotificationType.GigCanceled
+            };
 
-            //var attendees = _db.Attendances
-            //    .Where(a => a.GigId == gig.Id)
-            //    .Select(a => a.Attendee)
-            //    .ToList();
+            var attendees = _db.Attendances
+                .Where(a => a.GigId == gig.Id)
+                .Select(a => a.Attendee)
+                .ToList();
 
-            //foreach (var attendee in attendees)
-            //{
-            //    var userNotification = new UserNotification
-            //    {
-            //        User = attendee,
-            //        Notification = notification
-            //    };
-            //    _db.UserNotifications.Add(userNotification);
-            //}
+            foreach (var attendee in attendees)
+            {
+                var userNotification = new UserNotification
+                {
+                    User = attendee,
+                    Notification = notification
+                };
+                _db.UserNotifications.Add(userNotification);
+            }
 
             _db.SaveChanges();
 
