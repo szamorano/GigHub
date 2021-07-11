dc77e8e2adf24d0e56525ba0992ae116a85091f3
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-07-11 18:44:22 -0400

    Tell AutoMapper a mapping profile has been created

--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using AutoMapper;
+using GigHub.App_Start;
+using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Http;
 
 namespace GigHub
 {
@@ -9,6 +11,7 @@ namespace GigHub
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
