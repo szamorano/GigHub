c90033564eac9d6c0388d062c1bb69e1389d2a71
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-07-13 20:10:26 -0400

    Format dates in notifications using moment.js

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace GigHub
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore-umd-min.js"));
+                        "~/Scripts/underscore-umd-min.js",
+                        "~/Scripts/moment.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
