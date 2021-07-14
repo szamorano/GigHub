1d98f22af4ad7cb06146253124f117ce0913c3d1
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-07-14 19:11:32 -0400

    Implement bookmarkable pages search

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -15,18 +15,25 @@ namespace GigHub.Controllers
         {
             _db = new ApplicationDbContext();
         }
-        public ActionResult Index()
+        public ActionResult Index(string query = null)
         {
             var upcomingGigs = _db.Gigs
                 .Include(g => g.Artist)
                 .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now && !g.IsCanceled);
 
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                upcomingGigs = upcomingGigs
+                    .Where(g => g.Artist.Name.Contains(query) || g.Genre.Name.Contains(query) || g.Venue.Contains(query));
+            }
+
             var viewModel = new GigsViewModel
             {
                 UpcomingGigs = upcomingGigs,
                 ShowActions = User.Identity.IsAuthenticated,
-                Heading = "Upcoming Gigs"
+                Heading = "Upcoming Gigs",
+                SearchTerm = query
             };
 
             return View("Gigs", viewModel);
